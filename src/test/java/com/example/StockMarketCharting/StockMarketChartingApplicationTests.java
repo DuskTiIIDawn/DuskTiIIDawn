6001b9fe6a1175a008a381721a0a0d1873e623b0
Rahul Kushwaha <rkrahulkushwaha1999@gmail.com>
2021-06-21 14:44:05 +0530

    k

--- src/test/java/com/example/StockMarketCharting/StockMarketChartingApplicationTests.java
+++ src/test/java/com/example/StockMarketCharting/StockMarketChartingApplicationTests.java
@@ -2,25 +2,20 @@ package com.example.StockMarketCharting;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.StockMarketCharting.entities.Company;
 import com.example.StockMarketCharting.entities.StockCode;
 import com.example.StockMarketCharting.services.CompanyService;
 
-@SpringBootTest
 class StockMarketChartingApplicationTests {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	CompanyService service;
-
-	@Test
 
 	void contextLoads() {
 		Company company = service.findById(4L);
