bfc8fcb9cf08cf4d09d0110bdf4f3c629e17c906
Rahul Kushwaha <rkrahulkushwaha1999@gmail.com>
2021-06-21 13:14:44 +0530

    kkk

--- src/main/java/com/example/StockMarketCharting/StockMarketChartingApplication.java
+++ src/main/java/com/example/StockMarketCharting/StockMarketChartingApplication.java
@@ -1,14 +1,8 @@
 package com.example.StockMarketCharting;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EntityScan(basePackages = { "com.example.StockMarketCharting.entities" })
@@ -16,19 +10,6 @@ public class StockMarketChartingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockMarketChartingApplication.class, args);
-	}
-
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("https://stockmarketchartingfrontend.herokuapp.com"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin", "Cache-Control",
-				"Content-Type"));
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
 	}
 
 }
