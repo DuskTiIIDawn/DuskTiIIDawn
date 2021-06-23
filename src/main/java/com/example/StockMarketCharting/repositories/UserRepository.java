63540c5127308217f2c432b4273a3c7673c45f5f
Rahul Kushwaha <rkrahulkushwaha1999@gmail.com>
2021-06-23 19:35:06 +0530

    kkkk

--- src/main/java/com/example/StockMarketCharting/repositories/UserRepository.java
+++ src/main/java/com/example/StockMarketCharting/repositories/UserRepository.java
@@ -2,10 +2,15 @@ package com.example.StockMarketCharting.repositories;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.StockMarketCharting.entities.UserEntity;
 
+@Repository
+@Transactional
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
 	List<UserEntity> findByUserName(String userName);
