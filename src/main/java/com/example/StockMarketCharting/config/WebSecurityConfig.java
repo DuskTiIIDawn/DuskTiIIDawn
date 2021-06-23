c446dc9316fd6d8f35018426a06c47199b69c8e7
Rahul Kushwaha <rkrahulkushwaha1999@gmail.com>
2021-06-23 07:50:42 +0530

    OOOO

--- src/main/java/com/example/StockMarketCharting/config/WebSecurityConfig.java
+++ src/main/java/com/example/StockMarketCharting/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.StockMarketCharting.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.StockMarketCharting.jwt.JwtAuthenticationEntryPoint;
 import com.example.StockMarketCharting.jwt.JwtRequestFilter;
@@ -58,16 +63,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return filter;
 	}
 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("https://glacial-ridge-65812.herokuapp.com"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable().addFilterBefore(corsFilter(), SessionManagementFilter.class)
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers("/authenticate").permitAll().antMatchers("/setuserapi1").permitAll()
-				.antMatchers("/confirmuser/**").permitAll().antMatchers("/**").permitAll()
+				.antMatchers("/**").permitAll().antMatchers("/confirmuser/**").permitAll().antMatchers("/company/**")
+				.permitAll().antMatchers("/sector/**").permitAll().
 
 				// all other requests need to be authenticated
-				.anyRequest().authenticated().and().
+				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
 				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
