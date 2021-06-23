7efb954197950df2011915623d2d73f3f07cc711
Rahul Kushwaha <rkrahulkushwaha1999@gmail.com>
2021-06-23 06:50:55 +0530

    KKKK

--- src/main/java/com/example/StockMarketCharting/StockMarketChartingApplication.java
+++ src/main/java/com/example/StockMarketCharting/StockMarketChartingApplication.java
@@ -3,30 +3,13 @@ package com.example.StockMarketCharting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = { "com.example.StockMarketCharting.entities" })
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableWebSecurity
 public class StockMarketChartingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockMarketChartingApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("https://stockmarketchartingfrontend.herokuapp.com");
-			}
-		};
 	}
 
 }
--- src/main/java/com/example/StockMarketCharting/config/WebSecurityConfig.java
+++ src/main/java/com/example/StockMarketCharting/config/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
-		httpSecurity.cors().and().csrf().disable().addFilterBefore(corsFilter(), SessionManagementFilter.class)
+		httpSecurity.csrf().disable().addFilterBefore(corsFilter(), SessionManagementFilter.class)
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers("/authenticate").permitAll().antMatchers("/setuserapi1").permitAll()
 				.antMatchers("/confirmuser/**").permitAll()
