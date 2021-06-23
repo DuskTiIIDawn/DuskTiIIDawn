e63027e7255c106099a22ecdec9c8d187535a836
Rahul Kushwaha <rkrahulkushwaha1999@gmail.com>
2021-06-23 06:47:45 +0530

    KKK

--- src/main/java/com/example/StockMarketCharting/config/WebSecurityConfig.java
+++ src/main/java/com/example/StockMarketCharting/config/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.StockMarketCharting.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.session.SessionManagementFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.StockMarketCharting.jwt.JwtAuthenticationEntryPoint;
 import com.example.StockMarketCharting.jwt.JwtRequestFilter;
@@ -61,18 +56,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	SimpleCORSFilter corsFilter() {
 		SimpleCORSFilter filter = new SimpleCORSFilter();
 		return filter;
-	}
-
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("https://glacial-ridge-65812.herokuapp.com"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-		configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type"));
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
 	}
 
 	@Override
