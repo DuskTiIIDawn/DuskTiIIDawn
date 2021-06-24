672baa3b038ae8c38168c41657a7d1146f9171ac
Rahul Kushwaha <rkrahulkushwaha1999@gmail.com>
2021-06-24 13:59:14 +0530

    KKKK

--- src/main/java/com/example/StockMarketCharting/jwt/MailSender.java
+++ src/main/java/com/example/StockMarketCharting/jwt/MailSender.java
@@ -1,8 +1,6 @@
 
 package com.example.StockMarketCharting.jwt;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -14,8 +12,6 @@ public class MailSender {
 	@Autowired
 	JavaMailSender javaMailSender;
 
-	Logger logger = LoggerFactory.getLogger(this.getClass());
-
 	public void sendMail(String from, String to, String subject, String body) {
 
 		SimpleMailMessage mail = new SimpleMailMessage();
@@ -24,8 +20,7 @@ public class MailSender {
 		mail.setSubject(subject);
 		mail.setText(body);
 
-		logger.info("Sending...");
 		javaMailSender.send(mail);
-		logger.info("Done!");
+
 	}
 }
