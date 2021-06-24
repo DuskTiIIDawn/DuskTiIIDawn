bdf560fcd207589f25bf9369ee93908a9b9ed7ab
Rahul Kushwaha <rkrahulkushwaha1999@gmail.com>
2021-06-24 12:34:24 +0530

    KKKK

---
+++ src/main/java/com/example/StockMarketCharting/jwt/JavaMailerServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.StockMarketCharting.jwt;
+
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
+public class JavaMailerServiceImpl {
+
+	private MailSender mailSender;
+
+	public JavaMailerServiceImpl(JavaMailSenderImpl mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	public void sendMail(String to, String subject, String body) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(body);
+		mailSender.send(message);
+	}
+}
--- src/main/java/com/example/StockMarketCharting/jwt/UserEntityController.java
+++ src/main/java/com/example/StockMarketCharting/jwt/UserEntityController.java
@@ -2,15 +2,6 @@ package com.example.StockMarketCharting.jwt;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -40,6 +31,9 @@ public class UserEntityController {
 
 	@Autowired
 	private JwtUserDetailsService userDetailsService;
+
+	@Autowired
+	private JavaMailerServiceImpl mailService;
 
 	@RequestMapping(value = "/authenticate", method = RequestMethod.POST, headers = "Accept=application/json")
 	public Map<String, String> createAuthenticationToken(@RequestBody JsonNode request) throws Exception {
@@ -71,8 +65,7 @@ public class UserEntityController {
 
 	@RequestMapping(value = "/setuserapi1", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, String> userapi(@RequestBody UserEntity user, BindingResult result)
-			throws AddressException, MessagingException {
+	public Map<String, String> userapi(@RequestBody UserEntity user, BindingResult result) {
 
 		Map<String, String> res = new HashMap<>();
 
@@ -103,7 +96,7 @@ public class UserEntityController {
 				user.getEmail(), user.getMobileNumber(), user.isConfirmed());
 
 		service.saveUser(usr);
-		sendemail(usr.getId(), usr.getUserName(), usr.getEmail());
+		sendEmail(usr.getId(), usr.getUserName(), usr.getEmail());
 		res.put("OK", "User Created Succesfully ,Check your Mail To verify and Login!");
 		return res;
 
@@ -111,8 +104,7 @@ public class UserEntityController {
 
 	@RequestMapping(value = "/changePasswordapi1", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, String> changepasswordrapi(@RequestBody JsonNode request)
-			throws AddressException, MessagingException {
+	public Map<String, String> changepasswordrapi(@RequestBody JsonNode request) {
 
 		Map<String, String> res = new HashMap<>();
 		if (request.get("userName") == null) {
@@ -147,37 +139,12 @@ public class UserEntityController {
 		return service.findByUserName(request.get("userName").asText());
 	}
 
-	public void sendemail(Long uid, String uName, String email) throws AddressException, MessagingException {
+	public void sendEmail(Long uid, String uName, String email) {
 
-		final String username = "classesfuturetrack@gmail.com";
-		final String password = "aigixahfsjhixcpf";
-
-		Properties prop = new Properties();
-		prop.put("mail.smtp.host", "smtp.gmail.com");
-		prop.put("mail.smtp.port", "587");
-		prop.put("mail.smtp.auth", "true");
-		prop.put("mail.smtp.starttls.enable", "true");
-		// TLS
-
-		Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
-			protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-				return new javax.mail.PasswordAuthentication(username, password);
-			}
-		});
-		try {
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("classesfuturetrack@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-			message.setSubject("User confirmation email");
-			message.setText("From," + "\n\n Stock Market Charting App!");
-			message.setContent("<h1><a href =\"https://glacial-ridge-65812.herokuapp.com/confirmuser/" + uid + "/"
-					+ bcryptEncoder.encode(uName) + "/\"> Click to confirm </a></h1>", "text/html");
-			Transport.send(message);
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-
+		String subject = "Account Confirmation Mail from Stock Chart Marketing";
+		String message = "Click The link to confirm --> https://glacial-ridge-65812.herokuapp.com/confirmuser/" + uid
+				+ "/" + bcryptEncoder.encode(uName);
+		mailService.sendMail(email, subject, message);
 	}
 
 	@RequestMapping(value = "/confirmuser/{uid}/{encodedUsrName}", method = RequestMethod.GET)
