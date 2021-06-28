1dc297d0a990e6443a55a0abae89d0d3afe488da
Rahul Kushwaha <rkrahulkushwaha1999@gmail.com>
2021-06-28 17:57:43 +0530

    KKK

--- src/main/java/com/example/StockMarketCharting/controllers/StockCodeController.java
+++ src/main/java/com/example/StockMarketCharting/controllers/StockCodeController.java
@@ -149,7 +149,7 @@ public class StockCodeController {
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public String removeStockCode(@RequestBody JsonNode jsonNode) {
 		if (jsonNode.get("stockCodeId") == null) {
-			return "Stock  Code Id Must Not Be Null";
+			return "Stock Code Id Must Not Be Null";
 		}
 		Long stockCodeId = jsonNode.get("stockCodeId").asLong();
 		StockCode stockCode = service.findByStockCodeId(stockCodeId);
